fe3b1ff15280c080c195351a9fd5b43b192abac0
TheBlutarch <theblutarchmann@gmail.com>
2024-04-16 17:44:28 +0000

    PixysOS bringup

--- pixys_Pong.mk
+++ pixys_Pong.mk
@@ -11,14 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from Pong device
 $(call inherit-product, device/nothing/Pong/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixys stuff.
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_Pong
+PRODUCT_NAME := pixys_Pong
 PRODUCT_DEVICE := Pong
 PRODUCT_MANUFACTURER := nothing
 PRODUCT_BRAND := Nothing
 PRODUCT_MODEL := A065
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_SYSTEM_NAME := Pong
 PRODUCT_SYSTEM_DEVICE := Pong
