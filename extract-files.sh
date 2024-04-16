e8c5e913f54c7a01fa7502a5d6094f5973c9ac31
TheBlutarch <theblutarchmann@gmail.com>
2024-04-16 17:49:15 +0000

    Revert "Pong: Add firmware extraction support"
    This reverts commit f9134a3af72aeffc897eec14143514bde2dab28a.

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2023 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -77,7 +77,5 @@ twt_congestion_timeout=0
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-
-extract_firmware "${MY_DIR}/proprietary-firmware.txt" "${SRC}"
 
 "${MY_DIR}/setup-makefiles.sh"
